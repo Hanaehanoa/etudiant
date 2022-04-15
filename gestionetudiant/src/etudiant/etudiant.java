17614a48ca5c70c2b35f83d439d4c0ac136456d2
HANAE <hanaael2002@gmail.com>
2022-04-15 11:12:26 +0000

    constructeur

--- gestionetudiant/src/etudiant/etudiant.java
+++ gestionetudiant/src/etudiant/etudiant.java
@@ -4,9 +4,8 @@ public class etudiant {
  private int id;
  private String nom;
  private double note;
- 
- 
-	 
-	 
+  public etudiant (int id , String nom , double note ){
+     note =10;
+  }
  
 }
